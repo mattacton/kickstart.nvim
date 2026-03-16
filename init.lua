cde3a1b10f5937e3d65a8a8cb454aaeea5bb7c6e
Matt Acton <matt.acton@gmail.com>
2026-03-15 22:22:21 -0700

    Failed to add testing: Tests Not Found

--- init.lua
+++ init.lua
@@ -663,7 +663,35 @@ require('lazy').setup({
       end
     end,
   },
-
+  -- { -- neotest
+  --   'nvim-neotest/neotest',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-neotest/neotest-go',
+  --   },
+  --   opts = {},
+  --   config = function()
+  --     require('neotest').setup {
+  --       adapters = {
+  --         require 'neotest-go',
+  --       },
+  --       output_panel = {
+  --         enabled = true,
+  --         open = 'botright split | resize 15',
+  --       },
+  --       quickfix = {
+  --         open = false,
+  --       },
+  --     }
+  --
+  --     vim.keymap.set('n', '<leader>z', "<cmd>lua require('neotest').run.run()<CR>", { desc = 'Run Test' })
+  --     vim.keymap.set('n', '<leader>zf', "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", { desc = 'Run Test File' })
+  --     vim.keymap.set('n', '<leader>zp', "<cmd>lua require('neotest').output_panel.toggle()<CR>", { desc = 'Toggle Test Output Panel' })
+  --     vim.keymap.set('n', '<leader>zl', "<cmd>lua require('neotest').run.run_last()<CR>", { desc = 'Run Last Test' })
+  --     vim.keymap.set('n', '<leader>zs', "<cmd>lua require('neotest').summary.toggle()<CR>", { desc = 'Toggle Test Summary' })
+  --   end,
+  -- },
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
