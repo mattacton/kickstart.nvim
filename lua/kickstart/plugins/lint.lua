30bc02eb8b9d5355e534fe7e7bb17ea2f4b203da
Matt Acton <matt.acton@gmail.com>
2026-06-07 22:29:25 -0700

    Swap linter for markdown to rumdl

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -8,7 +8,7 @@ return {
   config = function()
     local lint = require 'lint'
     lint.linters_by_ft = {
-      markdown = { 'markdownlint' },
+      markdown = { 'rumdl' },
       javascript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
       typescript = { 'eslint_d' },
@@ -18,7 +18,7 @@ return {
     -- To allow other plugins to add linters to require('lint').linters_by_ft,
     -- instead set linters_by_ft like this:
     -- lint.linters_by_ft = lint.linters_by_ft or {}
-    -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+    -- lint.linters_by_ft['markdown'] = { 'rumdl' }
     --
     -- However, note that this will enable a set of default linters,
     -- which will cause errors unless these tools are available:
